8be9570ece302a1424757b05b69640d79d29db25
VitorRomanelli <vitorvromanelli@gmail.com>
2021-01-20 16:46:53 -0300

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" alt="Pokemon Logo">
+    <div class="header">
+      <img src="./assets/logo.png" alt="Pokemon Logo" />
+      <span> Pokedex </span>
+      <input
+        type="text"
+        placeholder="Search"
+        v-model="search"
+        id="search-input"
+      />
+      <button @click="searchPokemon">Search</button>
+    </div>
     <div class="pokemons">
-      <div v-for="(pokemon, index) in pokemons" :key="index">
-        <Pokemon :number="index + 1" :name="pokemon.name" />
+      <div v-for="(pokemon, index) in filter" :key="pokemon.url">
+        <Pokemon :number="index + 1" :name="pokemon.name" :url="pokemon.url" />
       </div>
     </div>
   </div>
@@ -19,20 +29,49 @@ export default {
   created: function () {
     api.get("pokemon?limit=151&offset=0").then((response) => {
       this.pokemons = response.data.results;
+      this.filter = response.data.results;
     });
   },
   data() {
     return {
+      search: "",
       pokemons: [],
+      filter: [],
     };
   },
   components: {
     Pokemon,
   },
+  methods: {
+    searchPokemon: function () {
+      this.filter = this.pokemons;
+      if (this.search === "" || this.search === " ") {
+        this.filter = this.pokemons;
+      } else {
+        this.filter = this.pokemons.filter(
+          (pokemon) => pokemon.name.toLowerCase() === this.search.toLowerCase()
+        );
+        console.log(this.filter);
+      }
+    },
+  },
+  computed: {
+    searchResult: function () {
+      if (this.search === "" || this.search === " ") {
+        return this.pokemons;
+      } else {
+        return this.pokemons.filter(
+          (pokemon) => pokemon.name.toLowerCase() === this.search.toLowerCase()
+        );
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Lobster&display=swap");
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -40,17 +79,114 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+
+.header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+  height: 240px;
+
+  box-sizing: border-box;
+}
+
+img {
+  width: 13%;
+}
+
+#search-input {
+  margin: 10px;
+  width: 40%;
+  height: 2rem;
+
+  border: none;
+  border-radius: 8px;
+
+  box-shadow: 0 0 0.8em #0004;
+  padding-left: 1.5rem;
+
+  outline: none;
+}
+
+#search-input:focus {
+  outline: 2px auto #ecd825;
+  box-shadow: none;
+}
+
+span {
+  margin: 15px;
+
+  font-family: "Lobster", cursive;
+  font-size: 28px;
+  font-weight: bold;
+
+  color: #fff;
+}
+
+button {
+  width: 20%;
+  height: 2rem;
+
+  outline: none;
+
+  border: none;
+  border-radius: 8px;
+
+  font-size: 16px;
+
+  background: #006ec9;
+  color: #fff;
+
+  transition: 300ms;
+}
+
+button:hover {
+  cursor: pointer;
+  background: #19db12;
+  width: 21%;
+
+  transition: 100ms;
+}
+
+button:focus {
+  border: 2px solid #ecd825;
+}
+
 .pokemons {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
 
   gap: 1.5rem;
 
   padding: 2rem;
+
+  height: 692px;
+
+  overflow-y: auto;
+  scroll-snap-type: x mandatory;
+  -webkit-overflow-scrolling: touch;
+  scroll-behavior: smooth;
+
+  margin: 20px;
+
+  box-sizing: border-box;
+  box-shadow: 0 0 1em #0004;
+
+  background-attachment: fixed;
 }
 
-img {
-  width: 50%;
+::-webkit-scrollbar {
+  display: none;
 }
 
+::-webkit-scrollbar-track {
+  background-color: #f4f4f4;
+  border-radius: 15px;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #dad7d7;
+  border-radius: 15px;
+}
 </style>>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -2,32 +2,51 @@
   <div class="pokemon">
     <img :src="currentImage" @click="changeImage" alt="" />
     <div class="stats">
-      <h1>{{ pokemon.name | firstLetterToUpperCase }}</h1>
+      <span>N° {{ pokemon.id }}</span>
+      <h1>{{ name | firstLetterToUpperCase }}</h1>
       <strong
-        >Type: {{ pokemon.types[0].type.name | firstLetterToUpperCase }}</strong
+        :class="{
+          'type-grass': pokemon.types[0].type.name === 'grass',
+          'type-fire': pokemon.types[0].type.name === 'fire',
+          'type-water': pokemon.types[0].type.name === 'water',
+          'type-bug': pokemon.types[0].type.name === 'bug',
+          'type-poison': pokemon.types[0].type.name === 'poison',
+          'type-normal': pokemon.types[0].type.name === 'normal',
+          'type-electric': pokemon.types[0].type.name === 'electric',
+          'type-ground': pokemon.types[0].type.name === 'ground',
+          'type-fairy': pokemon.types[0].type.name === 'fairy',
+          'type-fighting': pokemon.types[0].type.name === 'fighting',
+          'type-psychic': pokemon.types[0].type.name === 'psychic',
+          'type-rock': pokemon.types[0].type.name === 'rock',
+          'type-ghost': pokemon.types[0].type.name === 'ghost',
+        }"
+        >{{ pokemon.types[0].type.name | firstLetterToUpperCase }}</strong
       >
       <div class="physical-status">
         <strong>Height: {{ pokemon.height }}</strong>
         <strong>Weight: {{ pokemon.weight }}</strong>
       </div>
-      <span v-for="(status, index) in pokemon.stats" :key="index">
-        {{ status.stat.name }} {{ status.base_stat }}
-      </span>
+      <div class="pokestats">
+        <span v-for="(status, index) in pokemon.stats" :key="index">
+          {{ status.stat.name.toUpperCase() }}: {{ status.base_stat }}
+        </span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import api from "../services/api";
+import axios from "axios";
 
 export default {
   name: "Pokemon",
   props: {
     number: Number,
     name: String,
+    url: String,
   },
   created: function () {
-    api.get(`pokemon/${this.number}`).then((response) => {
+    axios.get(this.url).then((response) => {
       this.pokemon = response.data;
       this.currentImage = this.pokemon.sprites.front_default;
     });
@@ -51,7 +70,7 @@ export default {
         this.isFront = !this.isFront;
       } else {
         this.currentImage = this.pokemon.sprites.front_default;
-        this.isFront = !this.isFront;  
+        this.isFront = !this.isFront;
       }
     },
   },
@@ -59,32 +78,22 @@ export default {
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Lobster&display=swap");
+
 .pokemon {
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  background: #f1f1f1;
+  background: #0004;
 
   box-shadow: 0 0 1em #0004;
-
+  box-sizing: border-box;
   margin: 1.4rem;
 }
 
-.stats {
-  background: #d3d3d3;
-
-  padding: 0 12px 30px 12px;
-
-  width: 100%;
-  height: 50%;
-
-  border-radius: 6px;
-  box-shadow:1 1 1 1em #0004;
-}
-
 img {
-  width: 100%;
+  width: 97%;
   height: 50%;
 
   image-rendering: -moz-crisp-edges;
@@ -99,6 +108,111 @@ img {
 
 img:hover {
   cursor: pointer;
-  width: 105%;
+  width: 100%;
 }
+
+.stats {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background: #f5f5f5;
+
+  padding: 12px;
+
+  width: 100%;
+  height: 50%;
+
+  border-radius: 6px;
+  box-shadow: 1 1 1 1em #0004;
+}
+
+span {
+  align-self: flex-start;
+}
+
+h1 {
+  font-family: "Lobster", cursive;
+}
+
+strong {
+  width: 80%;
+  height: 1.2rem;
+
+  margin-bottom: 0.8rem;
+  border-radius: 6px;
+}
+
+strong.type-grass {
+  background: rgb(53, 211, 21);
+}
+
+strong.type-fire {
+  background: rgb(211, 59, 21);
+}
+
+strong.type-water {
+  background: rgb(21, 173, 211);
+}
+
+strong.type-bug {
+  background: rgb(122, 80, 0);
+  color: #fff;
+}
+
+strong.type-poison {
+  background: rgb(157, 21, 211);
+  color: #fff;
+}
+
+strong.type-normal {
+  background: rgb(197, 221, 210);
+}
+
+strong.type-electric {
+  background: rgb(255, 251, 17);
+}
+
+strong.type-ground {
+  background: rgb(170, 105, 19);
+  color: #fff;
+}
+
+strong.type-fighting {
+  background: rgb(107, 94, 59);
+}
+
+strong.type-fairy {
+  background: rgb(255, 83, 226);
+}
+
+strong.type-Psychic {
+  background: rgb(211, 21, 186);
+  color: #fff;
+}
+
+strong.type-rock {
+  background: rgb(59, 59, 59);
+  color: #fff;
+}
+
+strong.type-ghost {
+  background: rgb(255, 255, 255);
+}
+
+.physical-status {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+}
+
+.pokestats {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+
+  font-size: 12px;
+}
+
 </style>
